ce9dda55e8d3c309c4a7f77966f8d253201befba
Lukman10a <abdulrauflukman9@gmail.com>
2023-05-08 18:10:55 +0100

    add styles

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { useForm } from "react-hook-form";
-import { yupResolver } from "@rhookform/resolvers/yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 import tw from "twin.macro";
+// /** @jsxImportSource @emotion/react */
 
 const SignUp = () => {
   const schema = yup.object().shape({
@@ -11,7 +12,7 @@ const SignUp = () => {
     password: yup.string().min(5).max(25).required(),
     confirmPassword: yup
       .string()
-      .oneOf([yup.rec("password"), null])
+      .oneOf([yup.ref("password"), null])
       .required(),
   });
 
@@ -19,12 +20,12 @@ const SignUp = () => {
     resolver: yupResolver(schema),
   });
 
-  const OnSubmit = (data) => {
+  const onSubmit = (data) => {
     console.log(data);
   };
 
   return (
-    <form tw=" space-y-4 flex flex-col p-24" onSubmit={handleSubmit(OnSubmit)}>
+    <form tw="space-y-4 flex flex-col" onSubmit={handleSubmit(onSubmit)}>
       <fieldset tw="text-center">
         <h1 tw="text-[#271C0C] font-bold text-4xl">CREATE A SELLER ACCOUNT</h1>
         <p tw="font-medium text-lg">Start selling on Muslim Space today!</p>
@@ -72,8 +73,8 @@ const SignUp = () => {
           I accept the terms and privacy policy
         </label>
       </fieldset>
-      <button className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600">
-        Sign up
+      <button tw="bg-[#C89850] text-white py-2 px-4 rounded-md hover:bg-blue-600">
+        Create account
       </button>
     </form>
   );
