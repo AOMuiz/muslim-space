74decc8bb44ae60579f1a0fe164696a1c9baa002
AOMuiz <54303765+AOMuiz@users.noreply.github.com>
2023-05-10 11:47:11 +0100

    change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-green-[200] p-12">
+    <footer className='bg-green-[200] p-12'>
       <h3>Foooter</h3>
     </footer>
   );
