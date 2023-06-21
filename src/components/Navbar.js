1e1a70c62299432c74a3c49e78af6b25d6b959fb
AOMuiz <abdulwasiumuiz@gmail.com>
2023-06-21 22:56:23 +0100

    fix next/font not showing or working and minure design change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,17 +3,18 @@ import { IoLocationSharp } from "react-icons/io5";
 import { BsPerson } from "react-icons/bs";
 import { MdAddShoppingCart } from "react-icons/md";
 import { BsBell } from "react-icons/bs";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <>
-      <div tw="bg-gray-200 p-2 flex flex-row items-center justify-between">
+    <nav tw="border-b-2">
+      <div tw="bg-gray-200 p-2 flex flex-row items-center justify-between border-red-200">
         <div tw="flex flex-row items-center justify-center bg-white p-2 ml-6 border-[#C89850] rounded-lg">
           <IoLocationSharp />
           <p>Nigeria</p>
         </div>
         <div tw="flex flex-row items-center">
-          <div tw="bg-white border-2 [border-color: #C89850] rounded-bl-lg rounded-tl-lg">
+          <div tw="bg-white border-2 border-[#C89850] rounded-bl-lg rounded-tl-lg">
             <input
               type="text"
               placeholder="Search"
@@ -32,7 +33,7 @@ const Navbar = () => {
               <option value="cap">Cap</option>
             </select>
           </div>
-          <div tw="px-6 py-2 [background-color: #C89850] border-[#C89850] text-white rounded-tr-lg rounded-br-lg">
+          <div tw="px-6 py-2  bg-[#C89850] border-[#C89850] text-white rounded-tr-lg rounded-br-lg">
             <button>Search</button>
           </div>
         </div>
@@ -45,9 +46,10 @@ const Navbar = () => {
       </div>
 
       <div tw="flex text-center p-4 items-center justify-between text-lg gap-3">
-        <div>
-          <p tw="text-3xl">Muslim Space</p>
-        </div>
+        <p tw="text-3xl">
+          <Link href={"/"}>Muslim Space</Link>
+        </p>
+
         <div>
           <select name="" id="" tw="bg-white outline-none">
             <option value="products">Products</option>
@@ -62,15 +64,20 @@ const Navbar = () => {
             <option value="services&handwork">Services & Handwork</option>
           </select>
         </div>
-        <div tw="flex gap-6">
-          <button>Sign up</button>
-          <button>Sign in</button>
-          <button tw="[background-color: #C89850] text-white rounded-lg text-center justify-self-end p-2 mr-6">
+        <div tw="flex gap-6 items-center">
+          <Link href={"/signup"}>
+            <button>Sign Up</button>
+          </Link>
+          <Link href={"/signin"}>
+            <button>Sign in</button>
+          </Link>
+
+          <button tw="bg-[#C89850] text-white rounded-lg text-center justify-self-end p-2 mr-6">
             <p>Sell</p>
           </button>
         </div>
       </div>
-    </>
+    </nav>
   );
 };
 
